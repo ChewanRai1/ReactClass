ab24d3b7744a736d2269ec90e39eee581dc955de
ChewanRai1 <220154@softwarica.edu.np>
2024-04-25 10:27:22 +0545

    day 1 contains npx npx create-react-app react_app and Day 2 for routes for log in and register

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+import {BrowserRouter as Router,
+Routes,
+Route}from 'react-router-dom'
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path = '/register' element = {<h1>Register page</h1>} />
+        <Route path = '/login' element = {<h1>login page</h1>} />
+      </Routes>
+    </Router>
+
+  );
+}
+
+export default App;
