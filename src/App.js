f47437ab6e3d25e86a86f82830ba47d343af9a02
ChewanRai1 <220154@softwarica.edu.np>
2024-04-25 10:41:00 +0545

    For homepage creating separte page for homepage

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 import {BrowserRouter as Router,
 Routes,
 Route}from 'react-router-dom'
+import Homepage from './homepage/Homepage';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path = '/' element={<Homepage/>}/>
         <Route path = '/register' element = {<h1>Register page</h1>} />
         <Route path = '/login' element = {<h1>login page</h1>} />
       </Routes>
---
+++ src/homepage/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Homepage = () =>{
+    return (
+        <div>
+            <h1>Its Homepage</h1>
+        </div>
+    );
+}
+
+//exporitng 
+export default Homepage;
