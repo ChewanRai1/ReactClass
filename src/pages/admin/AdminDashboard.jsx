fca76d9d1d7bf86e32867f220e922aec83e8c6ce
ChewanRai1 <220154@softwarica.edu.np>
2024-05-20 15:39:55 +0545

    admin page all functions created

--- src/pages/admin/AdminDashboard.jsx
+++ src/pages/admin/AdminDashboard.jsx
@@ -3,9 +3,122 @@ import React from "react";
 const AdminDashboard = () => {
   return (
     <div className="container">
-      <div className="d-flex justify-content-between">
+      <div className="d-flex justify-content-between mt-2">
         <h2>Admin Dashboard</h2>
-        <button className="btn btn-danger">Add product</button>
+
+        <button
+          type="button"
+          class="btn btn-danger"
+          data-bs-toggle="modal"
+          data-bs-target="#exampleModal"
+        >
+          Add Product
+        </button>
+
+        <div
+          class="modal fade"
+          id="exampleModal"
+          tabindex="-1"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">
+                  Create a new product!
+                </h1>
+                <button
+                  type="button"
+                  class="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div class="modal-body">
+                <form action="">
+                  <label>Product Name</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Enter product name"
+                  ></input>
+
+                  <labe className="mt-2">Product Price</labe>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Enter product name"
+                  ></input>
+
+                  <div className="mt-2">
+                    <label>Select Category</label>
+                    <select className="form-control">
+                      <option value="flower">Plants</option>
+                      <option value="gadgets">Gadgets</option>
+                      <option value="electronics">Electronics</option>
+                      <option value="mobile">mobile</option>
+                    </select>
+                  </div>
+                  <label className="mt-2">Type product Description</label>
+                  <textarea className="form-control"></textarea>
+
+                  <label className="mt-2">Product image</label>
+                  <input type="file" className="form-control"></input>
+                </form>
+              </div>
+              <div class="modal-footer">
+                <button
+                  type="button"
+                  class="btn btn-secondary"
+                  data-bs-dismiss="modal"
+                >
+                  Close
+                </button>
+                <button type="button" class="btn btn-primary">
+                  Save changes
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <table className="table mt-13">
+          <thead className="table-dark">
+            <tr>
+              <th> Product Image </th>
+              <th> Product Name</th>
+              <th> Price</th>
+              <th> Category </th>
+              <th> Description</th>
+              <th> Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <img
+                  height={"40px"}
+                  width={"60px"}
+                  src="https://wallpapers.com/images/featured/tzhfez1w8ud2z8aw.jpg"
+                  alt="deadpool"
+                />
+              </td>
+              <td>Sunflower</td>
+              <td>Npr. 500</td>
+              <td>Superhero</td>
+              <td>Funny </td>
+              <td>
+                <div className="btn-group" role="group">
+                  <button className="btn btn-success">Edit</button>
+                  <button className="btn btn-danger">Delete</button>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   );
